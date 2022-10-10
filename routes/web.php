e5016226ab493734663983da05f4e045ff003360
Oscar Myo Min <oscarmyomin@gmail.com>
2022-10-10 19:14:24 +0630

    update history api

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/test', function(){
+    return "Hello";
+});
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
