a697710c6b0f37bcd5398d9f20a0a32c660360e9
Ko Htut <kohtut@192.168.1.53>
2023-01-13 07:20:53 +0700

    nova:theme change

---
+++ config/nova-snowball.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Sidebar Size
+  |--------------------------------------------------------------------------
+  |
+  | Set the sidebar width from a predefined list of templates.
+  |
+  | Possible values: 'thin', 'normal', 'thick'
+  |
+  */
+
+  'sidebarSize' => 'thin',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Collapse Sidebar Groups
+  |--------------------------------------------------------------------------
+  |
+  | Setting this value to true will collapse resource groups and expand them
+  | on selection collapsing any other expanded navigation items.
+  |
+  */
+
+  'sidebarCollapse' => true,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Sidebar Icons
+  |--------------------------------------------------------------------------
+  |
+  | Setting this value to false will hide sidebar icons for all navigation
+  | items and add trim some whitespace from each child navigation item.
+  |
+  */
+
+  'sidebarIcons' => true,
+
+];
